4eccf2723630e282b33f1ee56812dc65d383ef3d
DESKTOP-HB0C1U8\Usuario <gonzalo_cirilo@hotmail.com>
2018-01-12 22:39:28 -0500

    BFS

--- vol009/924_SpreadingTheNews.cpp
+++ vol009/924_SpreadingTheNews.cpp
@@ -4,28 +4,25 @@
 using namespace std;
 typedef vector<int>Vi;
 typedef vector<Vi>Gi;
-Gi grafo;
-Vi distancia;
-int e,m,u,c, vinicio;
-Vi dia;
-Vi cantpersonas;
+Gi graph;
+Vi distances;
+int e, m, u, c, vstart;
+Vi day;
+Vi people;
 void BFS(int nodo) {
-	queue<int> cola;
-	cola.push(nodo);
-	distancia[nodo] = 0;
-	cantpersonas[0] = 0;
-
-	while (!cola.empty()) {
-		int actual = cola.front();cola.pop();
-		for (int i = 0;i < grafo[actual].size();i++) {
-			int adyacente = grafo[actual][i];
-
-			if (distancia[adyacente] < 0) {
-				
-				distancia[adyacente] = distancia[actual] + 1;
-				int dia = distancia[adyacente];
-				cantpersonas[dia] += 1;
-				cola.push(adyacente);		
+	queue<int> q;
+	q.push(nodo);
+	distances[nodo] = 0;
+	people[0] = 0;
+	while (!q.empty()) {
+		int actual = q.front(); q.pop();
+		for (int i = 0; i < graph[actual].size(); i++) {
+			int adyacente = graph[actual][i];
+			if (distances[adyacente] < 0) {
+				distances[adyacente] = distances[actual] + 1;
+				int day = distances[adyacente];
+				people[day] += 1;
+				q.push(adyacente);
 			}
 		}
 	}
@@ -33,37 +30,34 @@ void BFS(int nodo) {
 int main()
 {
 	scanf("%d", &e);
-	grafo = Gi(e);
-	
-	for (int i = 0;i < e;i++) {
+	graph = Gi(e);
+
+	for (int i = 0; i < e; i++) {
 		scanf("%d ", &m);
-		for (int j = 0;j < m;j++) {
+		for (int j = 0; j < m; j++) {
 			scanf("%d ", &u);
-			grafo[i].push_back(u);
+			graph[i].push_back(u);
 		}
-		
 	}
-
 	scanf("%d", &c);
-	for (int j = 0;j < c;j++) {
-		scanf("%d", &vinicio);
-		distancia = Vi(e, -1);
-		cantpersonas = Vi(e, 0);
-		BFS(vinicio);
-
-		int menorDia = 0;
-		int boomEmp = cantpersonas[0];
-		for (int i = 1;i < e;i++) {
-			if (boomEmp < cantpersonas[i]) {
-				menorDia = i;
-				boomEmp = cantpersonas[i];
+	for (int j = 0; j < c; j++) {
+		scanf("%d", &vstart);
+		distances = Vi(e, -1);
+		people = Vi(e, 0);
+		BFS(vstart);
+		int mDay = 0;
+		int boomEmp = people[0];
+		for (int i = 1; i < e; i++) {
+			if (boomEmp < people[i]) {
+				mDay = i;
+				boomEmp = people[i];
 			}
 		}
-		if (menorDia == 0)
+		if (mDay == 0)
 			printf("0\n");
 		else {
-			printf("%d %d\n", boomEmp, menorDia);
+			printf("%d %d\n", boomEmp, mDay);
 		}
 	}
-    return 0;
+	return 0;
 }
