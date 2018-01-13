43c67444cc6d292e858672199bf11795a6e0d0b9
DESKTOP-HB0C1U8\Usuario <gonzalo_cirilo@hotmail.com>
2018-01-12 22:52:05 -0500

    DFS, recursive

--- vol100/10004_Bicoloring.cpp
+++ vol100/10004_Bicoloring.cpp
@@ -5,119 +5,77 @@ using namespace std;
 typedef vector<bool>Vb;
 typedef vector<int>Vi;
 typedef vector<Vi>Gi;
-Gi grafo;
-Vb visitados;
-int n, m,u,v;
+Gi graph;
+Vb visited;
+int n, m, u, v;
 Vi colors;
-stack<int>q;
+stack<int>pile;
 stack<int>pila;
-int maxcolors=4;
-//Variantes de coloracion de grafos
-//1.Minino de colores para colorear un grafo
-void asignarColor(int nodo) {
+int maxcolors = 4;
+void asignColor(int nodo) {
 	int color = 1;
 	while (true) {
 		int temp = color;
-		for (int i = 0; i < grafo[nodo].size(); i++) {
-			int adyacente = grafo[nodo][i];
-			if (colors[adyacente] == color) {
-				color = colors[adyacente] + 1;
+		for (int i = 0; i < graph[nodo].size(); i++) {
+			int ad = graph[nodo][i];
+			if (colors[ad] == color) {
+				color = colors[ad] + 1;
 				break;
 			}
 		}
 		if (temp == color) {
 			break;
-		}		
-	}
-	colors[nodo] = color;
-}
-void dfsColoring() {
-	while (!q.empty()) {
-		int nodo = q.top(); q.pop();
-		asignarColor(nodo);
-		visitados[nodo] = 1;
-		for (int i = 0; i < grafo[nodo].size(); i++) {
-			int adyacente = grafo[nodo][i];
-			if (visitados[adyacente] == 0) {
-				q.push(adyacente);
-			}
 		}
 	}
+	colors[nodo] = color;
 }
 void DFSbactrack(int nodo)
 {
-	asignarColor(nodo);
-	visitados[nodo] = true;
-	for (int i = 0; i < grafo[nodo].size(); i++)
+	asignColor(nodo);
+	visited[nodo] = true;
+	for (int i = 0; i < graph[nodo].size(); i++)
 	{
-		int adyacente = grafo[nodo][i];
-		if (visitados[adyacente] == false)
-			DFSbactrack(adyacente);
-		adyacente = 0;//limpiar memoria
+		int ad = graph[nodo][i];
+		if (visited[ad] == false)
+			DFSbactrack(ad);
+		ad = 0;
 	}
 	pila.push(nodo);
 }
-//fin variante 1\*/
-bool DFS(int nodo,int color) {
-	colors[nodo] = color;
-	for (int i = 0;i < grafo[nodo].size();i++)
-	{
-		int adyacente = grafo[nodo][i];
-		if (colors[adyacente] == 0) {
-			if (!DFS(adyacente, color*-1)) {
-				return false;
-				break;
-			}
-		} else{
-			if (colors[adyacente] == color) {
-				return false;
-				break;
-			}
-		}
-	}							
-	return true;
-}
 int main()
 {
-	Vi res=Vi();
+	Vi res = Vi();
 	Vi resp = Vi();
 	while (true) {
 		scanf("%d", &n);
 		if (n == 0)
 			break;
 		scanf("%d", &m);
-		grafo = Gi(n);
+		graph = Gi(n);
 		colors = Vi(n, 0);
-		visitados = Vb(n, false);
+		visited = Vb(n, false);
 		for (int i = 0; i < m; i++) {
 			scanf("%d %d", &u, &v);
-			grafo[u].push_back(v);
-			grafo[v].push_back(u);
+			graph[u].push_back(v);
+			graph[v].push_back(u);
 		}
 		for (int i = 0; i < n; i++) {
-			if (!visitados[i]) {
-				//q.push(i);
-
+			if (!visited[i]) {
 				DFSbactrack(i);
 			}
-		} 
+		}
 		int max = 0;
 		for (int n = 0; n < colors.size(); n++) {
 			if (colors.at(n) > max) {
 				max = colors.at(n);
 			}
-		}		
-		if (max<=2) {
+		}
+		if (max <= 2) {
 			printf("BICOLORABLE.\n");
 		}
 		else {
 			printf("NOT BICOLORABLE.\n");
 		}
-		
 	}
-
-	
-	
-
 	return 0;
 }
