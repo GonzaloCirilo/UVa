dac7f3ad7d52ba0c42be63fb74e39e72810202e8
DESKTOP-HB0C1U8\Usuario <gonzalo_cirilo@hotmail.com>
2018-01-18 23:24:32 -0500

    MST, UFDS

--- vol112/11228_TransportationSystem.cpp
+++ vol112/11228_TransportationSystem.cpp
@@ -7,55 +7,48 @@ using namespace std;
 typedef pair<double, pair<int, int>> iii;
 int n, r, x, y;
 vector<int>pset;
-vector<pair<int, int>>posiciones;
+vector<pair<int, int>>positions;
 void init() {
 	pset = vector<int>(n);
 	for (int i = 0; i < pset.size(); i++) {
 		pset[i] = i;
 	}
 }
-
-int findset(int nodo) {
-	if (nodo == pset[nodo])
-		return pset[nodo];
-	return pset[nodo] = findset(pset[nodo]);
+int findset(int node) {
+	if (node == pset[node])
+		return pset[node];
+	return pset[node] = findset(pset[node]);
 }
-
 bool isSameSet(int a, int b) {
 	return findset(a) == findset(b);
 }
-
 void unionset(int a, int b) {
 	if (!isSameSet(a, b))
 		pset[findset(a)] = findset(b);
 }
-double calcularDist(int u, int v) {
-	double dist = sqrt(pow(posiciones[v].first-posiciones[u].first,2) + pow(posiciones[v].second - posiciones[u].second,2));
-	return dist;
+double calDistance(int u, int v) {
+	return sqrt(pow(positions[v].first - positions[u].first, 2) + pow(positions[v].second - positions[u].second, 2));
 }
-priority_queue<iii, vector<iii>, greater<iii>>pq;
 int main()
 {
 	int t;
 	scanf("%d", &t);
-	for (int casos = 0; casos<t; casos++) {
+	for (int cases = 0; cases<t; cases++) {
 		scanf("%d %d", &n, &r);
 		init();
 		int cont = 0;
-		while (!pq.empty())pq.pop();
-		posiciones.clear();
+		priority_queue<iii, vector<iii>, greater<iii>>pq;
+		positions.clear();
 		for (int i = 0; i < n; i++) {
 			scanf("%d %d", &x, &y);
-			posiciones.push_back({ x,y });
+			positions.push_back({ x,y });
 		}
-		int a = 0;
 		for (int u = 0; u < n; u++) {
-			for (int v = 1+u; v < n; v++) {
-				a++;
-				pq.push({ calcularDist(u,v),{ u,v } });
+			for (int v = 1 + u; v < n; v++) {
+				pq.push({ calDistance(u,v),{ u,v } });
 			}
 		}
-		double mst1 = 0, mst2 = 0;int numStates = 1;
+		double mst1 = 0, mst2 = 0; int numStates = 1;
 		while (!pq.empty() && cont < n - 1) {
 			iii tri = pq.top(); pq.pop();
 			if (!isSameSet(tri.second.first, tri.second.second)) {
@@ -70,7 +63,7 @@ int main()
 				unionset(tri.second.first, tri.second.second);
 			}
 		}
-		printf("Case #%d: %d %d %d\n", casos + 1, numStates, (int)round(mst1), (int)round(mst2));
+		printf("Case #%d: %d %d %d\n", cases + 1, numStates, (int)round(mst1), (int)round(mst2));
 	}
 	return 0;
 }
