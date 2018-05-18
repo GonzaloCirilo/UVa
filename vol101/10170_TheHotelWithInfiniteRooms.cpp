c4b2325426c1babe2c321aba0d099493a76bb6f9
GonzaloCirilo <gonzalo_cirilo@hotmail.com>
2018-05-18 00:56:34 -0500

    Math: Traingular Root

--- vol101/10170_TheHotelWithInfiniteRooms.cpp
+++ vol101/10170_TheHotelWithInfiniteRooms.cpp
@@ -1,14 +1,11 @@
 #include <stdio.h>
 #include <math.h>
- 
-int main (){
+int main(){
     long long s, d;
- 
-    while ( scanf ("%lld %lld", &s, &d) != EOF ) {
+    while (scanf("%lld %lld", &s, &d) != EOF) {
         d = (d + (s * (s - 1)) / 2) * 2;
-        long long n = (long long)ceil ((sqrt (4 * d + 1) - 1) / 2.0);
-        printf ("%lld\n", n);
+        long long n = (long long) round(sqrt(d));
+        printf("%lld\n", n);
     }
- 
     return 0;
 }
