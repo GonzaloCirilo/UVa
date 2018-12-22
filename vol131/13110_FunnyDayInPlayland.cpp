b0f87497c5f14139625ca2896ede8821236cb6ef
GonzaloCirilo <gonzalo_cirilo@hotmail.com>
2018-12-21 23:41:25 -0500

    Euler Path, UFDS

--- vol131/13110_FunnyDayInPlayland.cpp
+++ vol131/13110_FunnyDayInPlayland.cpp
@@ -3,19 +3,17 @@
 #include <set>
 using namespace std;
 typedef vector<vector<int>>vvi;
-vector<int>pset;
-vvi at, type; vector<int>vd;
+int vd[10002];
+vvi at; 
+vector<int> type[10002], pset;
 int n, c, k, a, aux, nele = 0;
 void init(int n) {
-	pset = vector<int>(n);
+	pset = vector<int>(n, -1);
 	nele = n-1;
-	for (int i = 0; i < pset.size(); i++) {
-		pset[i] = i;
-	}
 }
 int findset(int nodo) {
-	if (nodo == pset[nodo])
-		return pset[nodo];
+	if (-1 == pset[nodo])
+		return nodo;
 	return pset[nodo] = findset(pset[nodo]);
 }
 bool isSameSet(int a, int b) {
@@ -33,23 +31,22 @@ bool check(int ev) {
 	for (int i = 0; i < c; i++) {
 		if (type[i].size()>1) {
 			for (int j = 0; j < type[i].size(); j++) {
-				int u = type[i][j], v, aux = j - 1;
-				if (j == 0)aux = type[i].size() - 1;
-				v = type[i][aux];
+				int u = type[i][j], v = j - 1 >= 0?j -1:type[i].size() - 1;
+				v = type[i][v];
 				if (u != ev&&v != ev)
 					unionset(u, v);
 			}			
 		}
 	}
-	return nele==1;
+	return nele == 1;
 }
 int main() {
 	while (scanf("%d %d", &n, &c) != EOF) {
-		at = vvi(n);int m = 9999999;
+		at = vvi(n);
+		int m = 9999999;
 		init(c);
-		vd = vector<int>(c,0);
-		type = vvi(c);
 		for (int ca = 0; ca < c; ca++) {
+			vd[ca] = 0;
 			scanf("%d", &k);
 			if (k == 0)nele--;
 			type[ca] = vector<int>(k);
@@ -60,7 +57,7 @@ int main() {
 				vd[ca] += 2;
 				if (at[aux].size() == 2) {
 					vd[at[aux][0]]--;
-					vd[at[aux][1]]--;
+					vd[at[aux][1]]--;	
 					unionset(at[aux][0], at[aux][1]);
 				}
 			}
@@ -93,17 +90,3 @@ int main() {
 	}
 	return 0;
 }
-/*
-8 6
-3 1 2 7
-3 0 1 3
-2 2 3
-3 0 4 6
-2 4 5
-2 5 6
-6 5
-2 2 1
-3 1 0 5
-2 3 4
-2 4 5
-3 0 3 2*/
