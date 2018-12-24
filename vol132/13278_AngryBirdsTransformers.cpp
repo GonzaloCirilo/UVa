944de89e61c014373c2119c7eeefc7b57b63afe0
Gonzalo Cirilo <gonzalo_cirilo@hotmail.com>
2018-12-24 15:32:47 -0500

    time optimization

--- vol132/13278_AngryBirdsTransformers.cpp
+++ vol132/13278_AngryBirdsTransformers.cpp
@@ -1,38 +1,25 @@
 #include <stdio.h>
 #include <vector>
 using namespace std;
-vector<int>starting;
-vector<int>ending;
+vector<int> starting, ending;
 int main(){
 	int n, x, y, sa=10001, ea=0;
-	while(true){
-		scanf("%d", &n);
-		if(n==0)break;
-		starting = vector<int>(10001);
-		ending = vector<int>(10001);
+	while(scanf("%d", &n) && n){
+		starting = vector<int>(10001, 0);
+		ending = vector<int>(10001,0);
 		for(int i = 0; i < n; i++){
 			scanf("%d %d", &x, &y);
-			if(x-y>0){
-				starting[x-y]+=1;
-				if(x-y<sa)sa=x-y;
-			}
-			else{
-				starting[0]+=1;
-				if(0<sa)sa=0;
-			}
-			if(x+y<10001){
-				ending[x+y]+=1;
-				if(x+y>ea)ea=x+y;	
-			}else{
-				if(10000>ea)ea=10000;
-			}
-
+			int dif = x - y > 0 ? x - y : 0, sum = x + y < 10001 ? x + y : 10000;
+			sa = sa > dif ? dif : sa;
+			starting[dif]++;
+			ea = ea < sum ? sum : ea;
+			ending[sum]++;
 		}
 		int ans = 0, sweep=0;
 		for(int i = sa; i <= ea; i++){
-			sweep+=starting[i];
-			if(ans<sweep)ans=sweep;
-			sweep-=ending[i];
+			sweep += starting[i];
+			ans = ans < sweep ? sweep : ans;
+			sweep -= ending[i];
 		}
 		printf("%d\n", ans);
 	}
