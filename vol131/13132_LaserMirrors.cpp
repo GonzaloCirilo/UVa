a621d443138cecb970ba0ed444ffe73ff77ba1cf
Gonzalo Cirilo <gonzalo_cirilo@hotmail.com>
2019-07-16 18:24:51 -0500

    Euler's Totient function

--- vol131/13132_LaserMirrors.cpp
+++ vol131/13132_LaserMirrors.cpp
@@ -1,6 +1,23 @@
 #include <stdio.h>
 int vphi[100001];
-int phi(int n){
+bool fastScan(int &x) {
+	bool neg = false;
+	int c = getchar();
+	bool end = c == EOF;
+	if (end)
+		return false;
+	x = 0;
+	if (c == '-') {
+		neg = true;
+		c = getchar();
+	}
+	for (; (c > 47 && c < 58); c = getchar())
+		x = (x << 1) + (x << 3) + c - 48;
+	if (neg)
+		x *= -1;
+	return true;
+}
+void phi(int n){
 	for (int i=1; i<=n; i++)
         vphi[i] = i; 
  
@@ -16,10 +33,10 @@ int phi(int n){
 
 int main(){
 	int t, n;
-	scanf("%d", &t);
+	scanf("%d\n", &t);
 	phi(100000);
 	while(t){
-		scanf("%d", &n);
+		fastScan(n);
 		printf("%d\n", vphi[n]);
 		t--;
 	}
