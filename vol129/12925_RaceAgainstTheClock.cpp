0485fe251b154ecddf0bf0517f4984931b2e07d5
Gonzalo Cirilo <gonzalo_cirilo@hotmail.com>
2019-09-19 09:58:44 -0500

    Fast Input, simulation

--- vol129/12925_RaceAgainstTheClock.cpp
+++ vol129/12925_RaceAgainstTheClock.cpp
@@ -8,13 +8,20 @@
 using namespace std;
 typedef pair<int, int> ii;
 int main() {
-	int t, n, d, q, x, sumtimes;
-	char s[301];
+	int t, n, d, q, x, sumtimes, si, j;
+	char s[303];
 	vector<int> remainDistance, times;
 	scanf("%d", &t);
 	while (t) {
-		scanf("%d %d", &n, &d);
-		scanf("%300s", &s);
+		scanf("%d %d\n", &n, &d);
+		int l = 0;
+		while (true) {
+			s[l++] = getchar();
+			if (s[l - 1] == '\n') {
+				l--;
+				break;
+			}
+		}
 		scanf("%d", &q);
 		for (int k = 0; k < q; k++) {
 			priority_queue<ii, vector<ii>, greater<ii>>order;
@@ -22,17 +29,17 @@ int main() {
 			sumtimes = 0;
 			times = vector<int>();
 			remainDistance = vector<int>(n, d);
-			int l = strlen(s);
-			for (int i = 0; i < strlen(s); i++) {
-				int si = (int)(s[i] - '0'), j = si - 1;
-				if (i + 1 >= 1 + j * x && s[si - 1]) {
-					remainDistance[si - 1]--;
-					if (!remainDistance[si - 1]) {
+			for (int i = 0; i < l; i++) {
+				si = (int)(s[i] - '0'), j = si - 1;
+				if (i + 1 >= 1 + j * x && s[j]) {
+					remainDistance[j]--;
+					if (!remainDistance[j]) {
 						order.push({ i + 1 - (j * x), si });
 						times.push_back(i + 1 - (j * x));
 						sumtimes += i + 1 - (j * x);
 					}
 				}
+				if (times.size() == n) break;
 			}
 			if (times.size() > 0) {
 				sort(times.begin(), times.end());
