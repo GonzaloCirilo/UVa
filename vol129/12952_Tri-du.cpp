46ef5917ecb3de0b04ebf7575c6eb7645f7a3431
Gonzalo Cirilo <gonzalo_cirilo@hotmail.com>
2019-11-09 15:40:11 -0500

    max

---
+++ vol129/12952_Tri-du.cpp
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <algorithm>
+using namespace std;
+int main(){
+    int a, b;
+    while(scanf("%d %d", &a, &b) != EOF){
+        printf("%d\n", max(a, b));
+    }
+    return 0;
+}
