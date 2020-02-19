9917f10362bc4889f1188d55df44e400b2a86707
Gonzalo Cirilo <gonzalo_cirilo@hotmail.com>
2020-02-18 23:00:49 -0500

    exp by squaring, pisano period:

---
+++ vol115/11582_ColossalFibonacciNumbers!.cpp
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <vector>
+using namespace std;
+typedef unsigned long long ull;
+typedef vector<ull> vll;
+
+ull squareExpMod(ull n, ull e, ull m){
+    if(e == 0) return 1 % m;
+    if(e % 2 == 0){
+        ull aux = squareExpMod(n, e / 2, m);
+        return ((aux % m) * (aux % m)) % m;
+    }else{
+        return ((n % m) * (squareExpMod(n, e - 1, m) % m)) % m;
+    }
+}
+
+vll getCycle(int m){
+    if(m == 1) return vll(1,0);
+    int cont = 0;
+    vll cycle = vll();
+    cycle.push_back(0);
+    cycle.push_back(1);
+    while(true){
+        cycle.push_back((cycle[cont] + cycle[cont + 1]) % m);
+        cont++;
+        if(cycle[cont] == 0 && cycle[cont + 1] == 1){
+            cycle.pop_back();
+            cycle.pop_back();
+            return cycle;
+        }
+    }
+}
+
+int main(){
+    int n, t;
+    ull a, b;
+    scanf("%d", &t);
+    while(t--){
+        scanf("%llu %llu %d", &a, &b, &n);
+        vll c = getCycle(n);
+        ull index = squareExpMod(a, b, c.size());
+        printf("%llu\n", c[index]);
+    }
+    return 0;
+}
