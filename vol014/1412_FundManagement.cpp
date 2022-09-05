14f3ac549097ff8d62223d07e0bb3bc1c64495c9
Gonzalo Cirilo <gonzalo_cirilo@hotmail.com>
2022-09-04 22:39:36 -0500

    time

---
+++ vol014/1412_FundManagement.cpp
@@ -0,0 +1,13 @@
+#include <vector>
+#include <iostream>
+using namespace std;
+int main(){
+    double c;
+    int m, n, k;
+    while(cin >> c >> m >> n >> k) {
+        for(int i = 0; i < n; i++){
+            
+        }
+    }
+    return 0;
+}
